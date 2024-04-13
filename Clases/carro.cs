25e248a26ec923b6ef35e5dae61c28b621dc258c
Rene Osorio <158326425+aleosorio22@users.noreply.github.com>
2024-04-13 16:01:15 -0600

    Add files via upload
    repo clase 8 POO progra 1

---
+++ Clases/carro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase8_POO.Clases
+{
+    public class Carro
+    {
+        public string Marca { get; }
+        public int Modelo { get;}
+        public string Color { get; set; }
+        public string Owner { get; set; }
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+        public int MAXVELOCIDAD {  get; set; }
+
+        public Carro(string m, int model)
+        {
+            Marca = m;
+            Modelo = model;
+        }
+
+        public int Acelerar()
+        {
+            if (Encendido == 0)
+            {
+                return 0;
+            }
+            velocidad_actual += 10;
+            return velocidad_actual;
+        }
+
+        public string EncenderCarro()
+        {
+            if (Encendido != 1)
+            {
+                Encendido = 1;
+            }
+            return "encendido";
+        }
+
+        public string ApagarCarro()
+        {
+            if (velocidad_actual != 0)
+            {
+                Encendido = 0;
+            }
+            return "Carro apagado";
+   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using clase8_POO.Clases;
+
+Carro carrito = new Carro("toyota", 2021);
+Carro car2 = new Carro("Nissan",2023 );
+
+
+carrito.Color = "blanco";
+carrito.Owner = "Jorge";
+
+Console.WriteLine($"Marca: {carrito.Marca}, modelo: {carrito.Modelo}");
+Console.WriteLine($"Marca carro 2: {car2.Marca}, modelo: {car2.Modelo}");
+
+if (car2.Acelerar() == 0)
+{
+    Console.WriteLine("El carro esta apagado");
+}
+else
+{
+    Console.WriteLine(car2.Acelerar() + "/kph");
+    Console.WriteLine(car2.Acelerar() + "/kph");
+}
+
+Console.WriteLine($"Encendiendo el carro...");
+car2.EncenderCarro();
+Console.WriteLine(car2.Acelerar() + "/kph");
+Console.WriteLine(car2.Acelerar() + "/kph");
+Console.WriteLine(car2.Acelerar() + "/kph");
+
+Console.WriteLine($"Apagando el carro... {car2.ApagarCarro}");
+
